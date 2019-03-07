ed089d4df26fbb6eda675138f832494dcbcec570
Uesli Almeida <uesli.ti@gmail.com>
2019-03-07 07:56:24 -0300

    Minor bug fixes

--- app/models/case_dao.js
+++ app/models/case_dao.js
@@ -1,23 +1,23 @@
 // recebe a conexão do banco e guarda numa var do objeto
-function caseDao(connection) {
-  this._connection = connection;
+function caseDao (connection) {
+  this._connection = connection
 }
 
 // método realiza a gravação de um caso já existe
-caseDao.prototype.saveCase = function(caseInfo, callback) {
-  var id = caseInfo.id_caso;
-  var nomeUsuario = caseInfo.usuario;
-  var numeroProcesso = caseInfo.numero_processo;
-  var violacaoDiretos = caseInfo.violacao_direitos;
-  var nome_agressor = caseInfo.nome_agressor;
-  var dataInclusao = caseInfo.data_inclusao;
-  var dataDesligamento = caseInfo.data_desligamento;
-  var encaminhamento = caseInfo.encaminhamento;
-  var situacao = caseInfo.situacao;
-  var consequencias = caseInfo.consequencias;
+caseDao.prototype.saveCase = function (caseInfo, callback) {
+  var id = caseInfo.id_caso
+  var nomeUsuario = caseInfo.usuario
+  var numeroProcesso = caseInfo.numero_processo
+  var violacaoDiretos = caseInfo.violacao_direitos
+  var nome_agressor = caseInfo.nome_agressor
+  var dataInclusao = caseInfo.data_inclusao
+  var dataDesligamento = caseInfo.data_desligamento
+  var encaminhamento = caseInfo.encaminhamento
+  var situacao = caseInfo.situacao
+  var consequencias = caseInfo.consequencias
 
   this._connection.query(
-    "UPDATE caso SET violacao=" +
+    'UPDATE caso SET violacao=' +
       "'" +
       violacaoDiretos +
       "', nome_agressor='" +
@@ -34,50 +34,50 @@ caseDao.prototype.saveCase = function(caseInfo, callback) {
       consequencias +
       "' WHERE id=" +
       id +
-      ";",
+      ';',
     callback
-  );
-};
+  )
+}
 
 // método realiza a gravação de um novo caso
-caseDao.prototype.saveNewCase = function(caseInfo, callback) {
-  var id = caseInfo.id_caso;
-  var nomeUsuario = caseInfo.usuario;
-  var numeroProcesso = caseInfo.numero_processo;
-  var violacaoDiretos = caseInfo.violacao_direitos;
-  var nomeAgressor = caseInfo.nome_agressor;
-  var dataInclusao = "";
-  var dataDesligamento = "";
+caseDao.prototype.saveNewCase = function (caseInfo, callback) {
+  var id = caseInfo.id_caso
+  var nomeUsuario = caseInfo.usuario
+  var numeroProcesso = caseInfo.numero_processo
+  var violacaoDiretos = caseInfo.violacao_direitos
+  var nomeAgressor = caseInfo.nome_agressor
+  var dataInclusao = ''
+  var dataDesligamento = ''
 
-  if (caseInfo.data_inclusao == "") {
-    dataInclusao = null;
+  if (caseInfo.data_inclusao == '') {
+    dataInclusao = null
   } else {
-    dataInclusao = "'" + caseInfo.data_inclusao + "'";
+    dataInclusao = "'" + caseInfo.data_inclusao + "'"
   }
 
-  if (caseInfo.data_desligamento == "") {
-    dataDesligamento = null;
+  if (caseInfo.data_desligamento == '') {
+    dataDesligamento = null
   } else {
-    dataDesligamento = "'" + caseInfo.data_desligamento + "'";
+    dataDesligamento = "'" + caseInfo.data_desligamento + "'"
   }
 
-  var encaminhamento = caseInfo.encaminhamento;
-  var situacao = caseInfo.situacao;
-  var consequencias = caseInfo.consequencias;
+  var encaminhamento = caseInfo.encaminhamento
+  var situacao = caseInfo.situacao
+  var consequencias = caseInfo.consequencias
 
   this._connection.query(
-    "INSERT INTO caso (id_usuario, numero_caso, violacao, nome_agressor, data_inclusao, encaminhamento, situacao, consequencias) VALUES((SELECT id FROM usuario WHERE nome = " +
+    'INSERT INTO caso (id_usuario, numero_caso, violacao, nome_agressor, data_inclusao, data_desligamento, encaminhamento, situacao, consequencias) VALUES((SELECT id FROM usuario WHERE nome = ' +
       "'" +
       nomeUsuario +
-      "'),'" +
+      "')," +
       numeroProcesso +
-      "','" +
+      ",'" +
       violacaoDiretos +
       "','" +
       nomeAgressor +
       "'," +
       dataInclusao +
-      ",'" +
+      ',' +
       dataDesligamento +
       ",'" +
       encaminhamento +
@@ -87,53 +87,53 @@ caseDao.prototype.saveNewCase = function(caseInfo, callback) {
       consequencias +
       "');",
     callback
-  );
-};
+  )
+}
 
 // método retorna todos os casos cadastros
-caseDao.prototype.getCases = function(callback) {
+caseDao.prototype.getCases = function (callback) {
   this._connection.query(
     'SELECT c.id as id_caso, id_usuario, u.nome as nome_usuario, numero_caso, violacao, nome_agressor, DATE_FORMAT(data_inclusao, "%d/%m/%Y") as data_inclusao, DATE_FORMAT(data_desligamento, "%d/%m/%Y") as data_desligamento, encaminhamento, situacao, consequencias FROM caso c LEFT JOIN usuario u ON c.id_usuario = u.id;',
     callback
-  );
-};
+  )
+}
 
 // recuerar os últimos casos Cadastrados
-caseDao.prototype.getLastCases = function(callback) {
+caseDao.prototype.getLastCases = function (callback) {
   this._connection.query(
     'SELECT c.id as id_caso, id_usuario, u.nome as nome_usuario, numero_caso, violacao, nome_agressor, DATE_FORMAT(data_inclusao, "%d/%m/%Y") as data_inclusao, DATE_FORMAT(data_desligamento, "%d/%m/%Y") as data_desligamento, encaminhamento, situacao, consequencias FROM caso c LEFT JOIN usuario u ON c.id_usuario = u.id  ORDER BY c.id DESC LIMIT 10;',
     callback
-  );
-};
+  )
+}
 
 // recupera os casos com data de desligamento nulo (casos ligados)
-caseDao.prototype.getOpenCases = function(callback) {
+caseDao.prototype.getOpenCases = function (callback) {
   this._connection.query(
     'SELECT c.id as id_caso, id_usuario, u.nome as nome_usuario, numero_caso, violacao, nome_agressor, DATE_FORMAT(data_inclusao, "%d/%m/%Y") as data_inclusao, DATE_FORMAT(data_desligamento, "%d/%m/%Y") as data_desligamento, encaminhamento, situacao, consequencias FROM caso c LEFT JOIN usuario u ON c.id_usuario = u.id WHERE c.data_desligamento IS null;',
     callback
-  );
-};
+  )
+}
 
 // método que recupera um caso específico a partir de um id
-caseDao.prototype.getCaseById = function(caseId, callback) {
+caseDao.prototype.getCaseById = function (caseId, callback) {
   this._connection.query(
     'SELECT c.id as id_caso, id_usuario, u.nome as nome_usuario, nome_responsavel, DATE_FORMAT(u.data_nascimento, "%d/%m/%Y") as data_nascimento, escolaridade, religiao, ano, endereco_rua, endereco_bairro, telefone, sexo, round(datediff(now(), u.data_nascimento) / 365.25) as idade, identificacao, beneficios, numero_caso, violacao, nome_agressor, DATE_FORMAT(data_inclusao, "%Y-%m-%d") as data_inclusao, DATE_FORMAT(data_desligamento, "%Y-%m-%d") as data_desligamento, encaminhamento, situacao, consequencias FROM caso c LEFT JOIN usuario u ON c.id_usuario = u.id WHERE c.id = ' +
       caseId,
     callback
-  );
-};
+  )
+}
 
 // método que recupera casos a partir de um número
-caseDao.prototype.getCaseByNumber = function(caseNumber, callback) {
+caseDao.prototype.getCaseByNumber = function (caseNumber, callback) {
   this._connection.query(
     'SELECT c.id as id_caso, id_usuario, u.nome as nome_usuario, nome_responsavel, DATE_FORMAT(u.data_nascimento, "%d/%m/%Y") as data_nascimento, escolaridade, religiao, ano, endereco_rua, endereco_bairro, telefone, sexo, round(datediff(now(), u.data_nascimento) / 365.25) as idade, identificacao, beneficios, numero_caso, violacao, nome_agressor, DATE_FORMAT(data_inclusao, "%Y-%m-%d") as data_inclusao, DATE_FORMAT(data_desligamento, "%Y-%m-%d") as data_desligamento, encaminhamento, situacao, consequencias FROM caso c LEFT JOIN usuario u ON c.id_usuario = u.id WHERE c.numero_caso = ' +
       caseNumber,
     callback
-  );
-};
+  )
+}
 
 // método que recupera casos em um período específico
-caseDao.prototype.getCaseByDate = function(
+caseDao.prototype.getCaseByDate = function (
   column,
   initialDate,
   finalDate,
@@ -150,9 +150,9 @@ caseDao.prototype.getCaseByDate = function(
       finalDate +
       "'",
     callback
-  );
-};
+  )
+}
 
-module.exports = function() {
-  return caseDao;
-};
+module.exports = function () {
+  return caseDao
+}
--- app/models/user_dao.js
+++ app/models/user_dao.js
@@ -1,32 +1,38 @@
 // recebe a conexão do banco e guarda numa var do objeto
-function userDao(connection) {
-  this._connection = connection;
+function userDao (connection) {
+  this._connection = connection
 }
 
 // método que salva um novo usuário no banco de dados
-userDao.prototype.saveNewUser = function(user, callback) {
-  var nome = user.nome;
-  var nomeResponsavel = user.nome_responsavel;
-  var endereco = user.endereco_rua;
-  var bairro = user.endereco_bairro;
-  var telefone = user.telefone;
-  var sexo = "";
+userDao.prototype.saveNewUser = function (user, callback) {
+  var nome = user.nome
+  var nomeResponsavel = user.nome_responsavel
+  var endereco = user.endereco_rua
+  var bairro = user.endereco_bairro
+  var telefone = user.telefone
+  var sexo = ''
 
-  if (user.sexo === "Masculino") {
-    sexo = "M";
+  if (user.sexo === 'Masculino') {
+    sexo = 'M'
   } else {
-    sexo = "F";
+    sexo = 'F'
   }
 
-  var dataNascimento = user.data_nascimento;
-  var escolaridade = user.escolaridade;
-  var religiao = user.religiao;
-  var ano = user.ano;
-  var identificacao = user.identificacao;
-  var beneficios = user.beneficios;
+  var dataNascimento = user.data_nascimento
+  var escolaridade = user.escolaridade
+  var religiao = user.religiao
+
+  var ano = null
+
+  if (user.ano !== '') {
+    ano = user.ano
+  }
+
+  var identificacao = user.identificacao
+  var beneficios = user.beneficios
 
   this._connection.query(
-    "INSERT INTO usuario (nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, data_nascimento, escolaridade, religiao, ano, identificacao, beneficios) VALUES(" +
+    'INSERT INTO usuario (nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, data_nascimento, escolaridade, religiao, ano, identificacao, beneficios) VALUES(' +
       "'" +
       nome +
       "','" +
@@ -45,42 +51,48 @@ userDao.prototype.saveNewUser = function(user, callback) {
       escolaridade +
       "','" +
       religiao +
-      "','" +
+      "'," +
       ano +
-      "','" +
+      ",'" +
       identificacao +
       "','" +
       beneficios +
       "');",
     callback
-  );
-};
+  )
+}
 
 // método que salva a edição de um usuário no banco de dados
-userDao.prototype.saveUser = function(user, callback) {
-  var id = user.id_usuario;
-  var nome = user.nome;
-  var nomeResponsavel = user.nome_responsavel;
-  var endereco = user.endereco_rua;
-  var bairro = user.endereco_bairro;
-  var telefone = user.telefone;
-  var sexo = "";
+userDao.prototype.saveUser = function (user, callback) {
+  var id = user.id_usuario
+  var nome = user.nome
+  var nomeResponsavel = user.nome_responsavel
+  var endereco = user.endereco_rua
+  var bairro = user.endereco_bairro
+  var telefone = user.telefone
+  var sexo = ''
 
-  if (user.sexo === "Masculino") {
-    sexo = "M";
+  if (user.sexo === 'Masculino') {
+    sexo = 'M'
   } else {
-    sexo = "F";
+    sexo = 'F'
   }
 
-  var dataNascimento = user.data_nascimento;
-  var escolaridade = user.escolaridade;
-  var religiao = user.religiao;
-  var ano = user.ano;
-  var identificacao = user.identificacao;
-  var beneficios = user.beneficios;
+  var dataNascimento = user.data_nascimento
+  var escolaridade = user.escolaridade
+  var religiao = user.religiao
+
+  var ano = null
+
+  if (user.ano !== '') {
+    ano = user.ano
+  }
+
+  var identificacao = user.identificacao
+  var beneficios = user.beneficios
 
   this._connection.query(
-    "UPDATE usuario SET nome=" +
+    'UPDATE usuario SET nome=' +
       "'" +
       nome +
       "', nome_responsavel='" +
@@ -99,36 +111,36 @@ userDao.prototype.saveUser = function(user, callback) {
       escolaridade +
       "', religiao='" +
       religiao +
-      "', ano='" +
+      "', ano=" +
       ano +
-      "', identificacao='" +
+      ", identificacao='" +
       identificacao +
       "', beneficios='" +
       beneficios +
       "' WHERE id=" +
       id +
-      ";",
+      ';',
     callback
-  );
-};
+  )
+}
 
 // método que recupera todos os usuários cadastrados
-userDao.prototype.getUsers = function(callback) {
+userDao.prototype.getUsers = function (callback) {
   this._connection.query(
     'SELECT id, nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, DATE_FORMAT(data_nascimento, "%d/%m/%Y") AS data_nascimento, escolaridade, religiao, ano, identificacao, beneficios FROM usuario;',
     callback
-  );
-};
+  )
+}
 
 // método que recupera todos os usuários que atendem os parâmetros da busca
-userDao.prototype.getUsersOnSearch = function(column, keyword, callback) {
-  if (column == "idade") {
+userDao.prototype.getUsersOnSearch = function (column, keyword, callback) {
+  if (column == 'idade') {
     this._connection.query(
       "SELECT id, nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, DATE_FORMAT(data_nascimento, '%d/%m/%Y') AS data_nascimento, escolaridade, religiao, ano, identificacao, beneficios FROM usuario WHERE round(datediff(now(), data_nascimento)/365.25) = " +
         parseInt(keyword) +
-        ";",
+        ';',
       callback
-    );
+    )
   } else {
     this._connection.query(
       "SELECT id, nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, DATE_FORMAT(data_nascimento, '%d/%m/%Y') AS data_nascimento, escolaridade, religiao, ano, identificacao, beneficios FROM usuario WHERE " +
@@ -137,27 +149,27 @@ userDao.prototype.getUsersOnSearch = function(column, keyword, callback) {
         keyword +
         "%';",
       callback
-    );
+    )
   }
-};
+}
 
 // método que recupera os últimos usuários cadastrados
-userDao.prototype.getLastUsers = function(callback) {
+userDao.prototype.getLastUsers = function (callback) {
   this._connection.query(
     'SELECT id, nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, DATE_FORMAT(data_nascimento, "%d/%m/%Y") AS data_nascimento, escolaridade, religiao, ano, identificacao, beneficios FROM usuario ORDER BY id DESC LIMIT 10;',
     callback
-  );
-};
+  )
+}
 
 // método que recupera um usuário específico a partir de um id
-userDao.prototype.getUserById = function(userId, callback) {
+userDao.prototype.getUserById = function (userId, callback) {
   this._connection.query(
     'SELECT id, nome, nome_responsavel, endereco_rua, endereco_bairro, telefone, sexo, DATE_FORMAT(data_nascimento, "%Y-%m-%d") AS data_nascimento, escolaridade, religiao, ano, identificacao, beneficios FROM usuario WHERE id = ' +
       userId,
     callback
-  );
-};
+  )
+}
 
-module.exports = function() {
-  return userDao;
-};
+module.exports = function () {
+  return userDao
+}
